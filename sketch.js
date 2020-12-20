c97be00403b8c44c2965aa6b88b6f315a22c5012
Vayema <ameya.voruganti@gmail.com>
2020-12-20 18:34:29 -0500

    hello

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var log6
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -35,7 +36,7 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
-
+    log6 = new Log(200,100,80,PI/2)
 }
 
 function draw(){
@@ -58,7 +59,8 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
-
+    log6.display();
+    
     bird.display();
     platform.display();
 }
